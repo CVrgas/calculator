c59dab8c3ec8d3630a9dcdb6d7d97d6ba9dbc0e2
CVrgas <usual389@gmail.com>
2023-06-19 15:43:57 -0400

    center calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,31 +2,69 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'calculator';
+  input = '';
+  display = "";
+  IsDone = false;
+  result = ''
+
+
+  numPress(num: string){
+    if(this.IsDone){
+      this.display = num;
+      this.input += num;
+      this.IsDone = false;
+      return;
+    }
+    this.input += num;
+    this.display = this.input;
+    
+  }
+  
+  simbolPress(simbol: string){
+
+    if(this.input == ""){
+      console.log ('Empty String')
+      return;
+    }
+
+    if(simbol == '.'){
+      this.numPress(simbol);
+      return
+    }
+
+    this.doMath();
+    this.input += simbol;
+    this.display = this.result;
+  }
+
+  clearAll(){
+    this.input = '';
+    this.display = "";
+    this.IsDone = false;
+    this.result = ''; 
+  }
+
+  doMath(){
+    let lastkey = this.result[this.result.length -1];
+    if( lastkey === '/' || lastkey === '*' || lastkey === '-' || lastkey === '+' || lastkey === '.' ) {
+      this.result = this.result.substring(0,this.result.length -1);
+    };
+    this.IsDone = true;
+    this.result = eval(this.input)
+  }
+
+
+  getResult(){
+    this.doMath();
+    this.display= this.result;
+    this.input = '';
+    this.IsDone = false;
+    this.result = '';
+
+  }
 }
